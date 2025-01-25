898c47ee835e1cad8f0db5045434a393329f192c
= <=>
2025-01-25 22:25:24 +0000

    feat: added unit test

---
+++ test/api/api.test.js
@@ -0,0 +1,10 @@
+import { assert } from "chai";
+import app from "../../app.js";
+import http from "http";
+
+describe("Test API", () => {
+   it("Get Genres", () => {
+      assert.isTrue(true);
+   });
+
+});
