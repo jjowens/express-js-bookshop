bcf6c0a77a855f869ef62c18cc307a7ce145d32f
= <=>
2025-02-10 07:55:12 +0000

    feat: wip, testing imports

--- README.md
+++ README.md
@@ -3,3 +3,9 @@ Personal project
 
 # url
 http://localhost:9060/
+
+
+# Stackoverflow
+
+Unit testing
+https://www.reddit.com/r/node/comments/1af6r52/help_with_chai/?rdt=32862
--- test/api/api.test.js
+++ test/api/api.test.js
@@ -1,10 +1,23 @@
-import { assert } from "chai";
-import app from "../../app.js";
-import http from "http";
+import { use, assert } from 'chai';
+import chaiHttp from 'chai-http';
+import * as server from '../../app.js';
+const chai = use(chaiHttp);
+
+chai.use(chaiHttp);
 
 describe("Test API", () => {
    it("Get Genres", () => {
-      assert.isTrue(true);
+
+      assert.isTrue(true, "Valid");
+      chai.request(server)
+          .get("/api/genres")
+          .set("Accept", "application/json")
+          .expect(200)
+          .end(function (err, res) {
+             if (err) {
+                assert.fail(err);
+             }
+          })
    });
 
 });
